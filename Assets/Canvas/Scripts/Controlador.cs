8d71478914782d55078d32d6c7f50c4d2ec4331b
Adriana Perez <adriana.perez@ucsp.edu.pe>
2020-05-03 14:49:36 -0500

    Menu
    Recursos para el menu

---
+++ Assets/Canvas/Scripts/Controlador.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Text;
+using UnityEngine.Windows.Speech;
+using System;
+using Vuforia;
+using UnityEngine.EventSystems;
+public class Controlador : MonoBehaviour
+{
+
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+
+    public GameObject PanelMenu;
+    public GameObject PanelJugar;
+    public GameObject PanelManual;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions.Add("Jugar", BotonJugar);
+        actions.Add("Manual", BotonManual);
+        actions.Add("Salir", BotonSalir);
+        actions.Add("Menu", BotonMenu);
+
+
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+
+    }
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    public void BotonManual()
+    {
+        PanelManual.SetActive(true);
+        PanelJugar.SetActive(false);
+        PanelMenu.SetActive(false);
+    }
+    public void BotonJugar()
+    {
+        PanelManual.SetActive(false);
+        PanelJugar.SetActive(true);
+        PanelMenu.SetActive(false);
+    }
+    public void BotonMenu()
+    {
+        PanelManual.SetActive(false);
+        PanelJugar.SetActive(false);
+        PanelMenu.SetActive(true);
+    }
+    public void BotonSalir()
+    {
+        print("salir");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Canvas/Scripts/SeleccionarPanel.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Text;
+using UnityEngine.Windows.Speech;
+using System;
+using UnityEngine.EventSystems;
+
+
+public class SeleccionarPanel : MonoBehaviour
+{
+
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+
+    public GameObject PanelEnemigo;
+    public GameObject PanelDemo;
+
+
+
+    bool VisibleEnemigo = false;
+    bool VisibleDemo = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions.Add("debilidades", BotonEnemigosPulsado);
+        actions.Add("demo", BotonDemoPulsado);
+        actions.Add("regresar", BotonRegresoEne);
+
+
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+
+    }
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    public void BotonEnemigosPulsado() 
+    {
+        if (VisibleEnemigo == false)
+        {
+            PanelEnemigo.SetActive(true);
+            VisibleEnemigo = true;
+        }
+    }
+
+    public void BotonDemoPulsado()
+    {
+        if (VisibleDemo == false)
+        {
+            PanelDemo.SetActive(true);
+            VisibleDemo = true;
+        }
+    }
+
+    public void BotonRegresoEne()
+    {
+        if (VisibleEnemigo ==true)
+        {
+
+            PanelEnemigo.SetActive(false);
+           
+            VisibleEnemigo = false;
+        }
+    }
+
+    public void BotonRegresoDemo()
+    {
+        if (VisibleDemo == true )
+        {
+            PanelDemo.SetActive(false);
+
+            VisibleDemo = false;
+           
+        }
+    }
+
+
+}
